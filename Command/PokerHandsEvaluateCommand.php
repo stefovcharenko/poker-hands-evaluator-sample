a374048a48f27d8962b2c850f8c45e142de1418a
Stefan Ovcharenko <ov4ar08@gmail.com>
2019-11-23 22:42:33 +0200

    app added

---
+++ Command/PokerHandsEvaluateCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Command;
+
+use Stefan\PokerHandsEvaluator\Output\StringOutput;
+use Stefan\PokerHandsEvaluator\Parser\TextSourceParser;
+use Stefan\PokerHandsEvaluator\PokerHandsEvaluator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputArgument, InputInterface};
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PokerHandsEvaluateCommand extends Command
+{
+    protected static $defaultName = 'app:evaluate-poker-hands';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Evaluates poker hands from specified file.')
+            ->addArgument('filePath', InputArgument::REQUIRED, 'Path to source file.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $evaluator = new PokerHandsEvaluator();
+        $evaluator
+            ->setOutput(new StringOutput())
+            ->setParser(new TextSourceParser($input->getArgument('filePath')));
+
+        $output->writeln($evaluator->process());
+    }
+}
